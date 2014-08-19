e04ee4b9e350edf3237d9096e9eb9dc03ebec5de
Andrew Gatlabayan <andrew.gatlabayan@gmail.com>
2014-08-18 22:34:56 -0700

    Switched file to four spaced tabs

--- test/index-spec.js
+++ test/index-spec.js
@@ -8,90 +8,90 @@ var sinon = require( 'sinon' );
 describe( '***REDACTED*** JS Log', function ( ) {
 
     before( function ( ) {
-      var self = this;
-      self.getInstance = function ( ) {
-        return require( '../index' );
-      };
+        var self = this;
+        self.getInstance = function ( ) {
+            return require( '../index' );
+        };
     } );
 
-    describe.only( 'Instantiation', function ( ) {
+    describe( 'Instantiation', function ( ) {
 
-      it( 'requires to be created with the new operator', function ( ) {
-        var Logger = this.getInstance();
-        var myLogger = null;
+        it( 'requires to be created with the new operator', function ( ) {
+            var Logger = this.getInstance();
+            var myLogger = null;
 
-        Logger.should.be.a.Function;
-        myLogger = new Logger();
+            Logger.should.be.a.Function;
+            myLogger = new Logger();
 
-        myLogger.should.be.an.Object;
-        myLogger.should.not.be.an.instanceOf( Logger );
+            myLogger.should.be.an.Object;
+            myLogger.should.not.be.an.instanceOf( Logger );
 
-      } );
+        } );
 
-      it( 'it does not require any parameters', function ( ) {
-        var Logger = this.getInstance();
-        Logger.bind( null, null ).should.not.throw();
-      } );
+        it( 'it does not require any parameters', function ( ) {
+            var Logger = this.getInstance();
+            Logger.bind( null, null ).should.not.throw();
+        } );
 
-      it( 'it returns a default logger if not parameters are passed', function ( ) {
-        var Logger = this.getInstance();
-        var myLogger = null;
+        it( 'it returns a default logger if not parameters are passed', function ( ) {
+            var Logger = this.getInstance();
+            var myLogger = null;
 
-        myLogger = new Logger();
-        myLogger.should.have.properties( [
-          'config',
-          'error',
-          'warn',
-          'debug',
-          'err',
-          'warning'
-        ] );
-      } );
+            myLogger = new Logger();
+            myLogger.should.have.properties( [
+                'config',
+                'error',
+                'warn',
+                'debug',
+                'err',
+                'warning'
+            ] );
+        } );
 
-      it( 'will have a method to output errors', function ( ) {
-        var Logger = this.getInstance();
-        var myLogger = null;
+        it( 'will have a method to output errors', function ( ) {
+            var Logger = this.getInstance();
+            var myLogger = null;
 
-        myLogger = new Logger();
+            myLogger = new Logger();
 
-        myLogger.error.should.be.a.Function;
-        myLogger.error( 'test' );
+            myLogger.error.should.be.a.Function;
+            myLogger.error( 'test' );
 
-        myLogger.err.should.be.a.Function;
-        myLogger.err( 'test' );
-      } );
+            myLogger.err.should.be.a.Function;
+            myLogger.err( 'test' );
+        } );
 
-      it( 'will have a method to output warnings', function ( ) {
-        var Logger = this.getInstance();
-        var myLogger = null;
+        it( 'will have a method to output warnings', function ( ) {
+            var Logger = this.getInstance();
+            var myLogger = null;
 
-        myLogger = new Logger();
+            myLogger = new Logger();
 
-        myLogger.warn.should.be.a.Function;
-        myLogger.warn( 'test' );
+            myLogger.warn.should.be.a.Function;
+            myLogger.warn( 'test' );
 
-        myLogger.warning.should.be.a.Function;
-        myLogger.warning( 'test' );
-      } );
+            myLogger.warning.should.be.a.Function;
+            myLogger.warning( 'test' );
+        } );
 
-      it( 'will have a method to output debug messages', function ( ) {
-        var Logger = this.getInstance();
-        var myLogger = null;
+        it( 'will have a method to output debug messages', function ( ) {
+            var Logger = this.getInstance();
+            var myLogger = null;
 
-        myLogger = new Logger();
+            myLogger = new Logger();
 
-        myLogger.debug.should.be.a.Function;
-        myLogger.debug( 'test' );
-      } )
+            myLogger.debug.should.be.a.Function;
+            myLogger.debug( 'test' );
+        } );
     } );
 
     describe( 'Configurations', function ( ) {
         it( 'accepts configuration object at instantiation', function ( ) {
-          var Logger = this.getInstance();
-          var myConfig = {
-            foo: bar
-          };
-          Logger.bind( null, myConfig ).should.not.throw();
+            var Logger = this.getInstance();
+            var myConfig = {
+                foo: 'bar'
+            };
+            Logger.bind( null, myConfig ).should.not.throw();
         } );
 
         it( 'accepts a toggle configuration' );
