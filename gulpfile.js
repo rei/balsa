532805c6cbecad2ad4b60a617f52543b712c2096
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2014-10-02 18:07:47 -0700

    Correct globs

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,12 @@ var mocha       = require( 'gulp-mocha' );
 
 // Test with coverage
 gulp.task( 'test', function ( cb ) {
-  gulp.src( [ 'lib/**/*.js', 'main.js' ] )
-    .pipe( istanbul() )
+  gulp.src( [ 'index.js', 'lib/**/*.js', 'relays/**/*.js' ] )
+    .pipe( istanbul( {
+        includeUntested: true
+    } ) )
     .on( 'finish', function () {
-      gulp.src( [ 'test/*.js' ] )
+      gulp.src( [ 'test/**/*.js' ] )
         .pipe( mocha() )
         .pipe( istanbul.writeReports( {
             reporters: [ 'text' ]
