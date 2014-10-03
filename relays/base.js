bc1f98c5051eb8486b76911633082b19534affb1
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2014-10-02 17:33:36 -0700

    Implement level limits

--- relays/base.js
+++ relays/base.js
@@ -1,4 +1,5 @@
-var util = require( '../lib/util' );
+var _isNull = require( 'lodash-node/compat/objects/isNull' );
+var util    = require( '../lib/util' );
 
 // Last relay ID
 var lastId = 0;
@@ -13,23 +14,31 @@ module.exports = function BaseRelay ( relayOpts ) {
 
     self.log = function ( logEvent, loggerConfig ) {
 
-        // Use message format from the relay, or the logger
-        var messageFormat = relayOpts.messageFormat || loggerConfig.messageFormat;
+        // Determine if this relay is enabled at this level
+        var minLevel        = relayOpts.minLevel || loggerConfig.minLevel;
+        var levelCode       = util.getLevelCode( logEvent.level, loggerConfig.levels );
+        var minLevelCode    = util.getLevelCode( minLevel, loggerConfig.levels );
+        var levelEnabled    = _isNull( minLevel ) || levelCode >= minLevelCode;
 
-        // Render message based on message format
-        var renderedMessage = messageFormat
-            .replace( '$TIMESTAMP', logEvent.timeStamp )
-            .replace( '$LEVEL',     logEvent.level )
-            .replace( '$PREFIX',    logEvent.prefix )
-            .replace( '$MESSAGE',   logEvent.rawMessage.join( ' ' ) );
+        if ( levelEnabled ) {
+            // Use message format from the relay, or the logger
+            var messageFormat = relayOpts.messageFormat || loggerConfig.messageFormat;
 
-        relayOpts.onLog( {
-            timestamp:          logEvent.timestamp,
-            level:              logEvent.level,
-            prefix:             loggerConfig.prefix,
-            rawMessage:         logEvent.rawMessage,
-            renderedMessage:    renderedMessage
-        } );
+            // Render message based on message format
+            var renderedMessage = messageFormat
+                .replace( '$TIMESTAMP', logEvent.timeStamp )
+                .replace( '$LEVEL',     logEvent.level )
+                .replace( '$PREFIX',    logEvent.prefix )
+                .replace( '$MESSAGE',   logEvent.rawMessage.join( ' ' ) );
+
+            relayOpts.onLog( {
+                timestamp:          logEvent.timestamp,
+                level:              logEvent.level,
+                prefix:             loggerConfig.prefix,
+                rawMessage:         logEvent.rawMessage,
+                renderedMessage:    renderedMessage
+            } );
+        }
     };
 
     return self;
