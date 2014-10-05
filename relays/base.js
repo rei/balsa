2b8a9455edec1d0ce731f93bbbcaf6cd56a601de
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2014-10-05 14:51:39 -0700

    Refactoring, reorganizing, documenting

--- relays/base.js
+++ relays/base.js
@@ -6,23 +6,59 @@ var lastId = 0;
 
 /**
  * Base relay. All relays should extend this.
+ *
+ * @param {object} opts         - Constructor options for a new relay
+ * @param {string} opts.onLog   - Callback for when a logging event occurs.
+ *                                Will be passed a log event, which includes:
+ *
+ *                                  - timestamp - Timestamp, in ISO 8601
+ *                                  - level     - Logging level
+ *                                  - prefix:   - Message prefix
+ *                                  - rawArgs:  - Raw logging arguments
+ *                                  - message:  - The full rendered log message
+ *
+ * @param {string} [opts.minLevel=Balsa.config.minLevel]
+ *                              - Minimum level to log at. Overrides global
+ *                                config for this relay.
+ *
+ * @param {string} [opts.messageFormat=Balsa.config.messageFormat]
+ *                              - Message format. Overrides global config for
+ *                                this relay.
+ *
+ * @param {string} [opts.prefix=Balsa.config.prefix]
+ *                              - Message prefix. Overrides global config for
+ *                                this relay.
  */
-module.exports = function BaseRelay ( relayOpts ) {
+module.exports = function BaseRelay ( opts ) {
     var self = {};
 
+    // Process options
+    if ( !_isFunction( opts.onLog ) ) {
+        throw new TypeError( 'Each relay must supply an `onLog` callback function.' );
+    }
+
+    // Give each instance a unique ID
     self.id = lastId++;
 
+    /**
+     * Callback function called by Balsa.
+     *
+     * @param  {[type]} logEvent     [description]
+     * @param  {[type]} loggerConfig [description]
+     * @return {[type]}              [description]
+     */
     self.log = function ( logEvent, loggerConfig ) {
 
         // Determine if this relay is enabled at this level
-        var minLevel        = relayOpts.minLevel || loggerConfig.minLevel;
-        var levelCode       = util.getLevelCode( logEvent.level, loggerConfig.levels );
-        var minLevelCode    = util.getLevelCode( minLevel, loggerConfig.levels );
+        var minLevel        = opts.minLevel || loggerConfig.minLevel;
+        var levelCode       = loggerConfig.levels.indexOf( logEvent.level );
+        var minLevelCode    = loggerConfig.levels.indexOf( minLevel );
         var levelEnabled    = _isNull( minLevel ) || levelCode >= minLevelCode;
 
         if ( levelEnabled ) {
+
             // Use message format from the relay, or the logger
-            var messageFormat = relayOpts.messageFormat || loggerConfig.messageFormat;
+            var messageFormat = opts.messageFormat || loggerConfig.messageFormat;
 
             // Render message based on message format
             var renderedMessage = messageFormat
@@ -31,13 +67,12 @@ module.exports = function BaseRelay ( relayOpts ) {
                 .replace( '$PREFIX',    logEvent.prefix )
                 .replace( '$MESSAGE',   logEvent.rawMessage.join( ' ' ) );
 
-            relayOpts.onLog( {
-                timestamp:          logEvent.timestamp,
-                level:              logEvent.level,
-                prefix:             loggerConfig.prefix,
-                rawMessage:         logEvent.rawMessage,
-                renderedMessage:    renderedMessage
-            } );
+            // Decorate log event with the rendered message and prefix
+            logEvent.prefix = opts.prefix || loggerConfig.prefix;
+            logEvent.message = renderedMessage;
+
+            // Call the relay's onLog function
+            opts.onLog( logEvent );
         }
     };
 
