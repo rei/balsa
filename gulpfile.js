4e78c0576962ba544cb52e1b17504a6e2cb2dcca
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2014-10-06 11:19:26 -0700

    Add option for coverage summary when testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,28 @@
 var gulp        = require( 'gulp' );
+var gutil       = require( 'gulp-util' );
 var istanbul    = require( 'gulp-istanbul' );
 var mocha       = require( 'gulp-mocha' );
 
-// Test with coverage
+// Run unit tets. Pass `--cover` if you'd also like a coverage report.
 gulp.task( 'test', function ( cb ) {
-  gulp.src( [ 'index.js', 'lib/**/*.js', 'relays/**/*.js' ] )
-    .pipe( istanbul( {
-        includeUntested: true
-    } ) )
-    .on( 'finish', function () {
-      gulp.src( [ 'test/**/*.js' ] )
-        .pipe( mocha() )
-        .pipe( istanbul.writeReports( {
-            reporters: [ 'text' ]
+
+    if ( !gutil.env.cover ) {
+        gulp.src( [ 'test/**/*.js' ] )
+            .pipe( mocha() )
+            .on( 'end', cb );
+    }
+
+    gulp.src( [ 'index.js', 'lib/**/*.js', 'relays/**/*.js' ] )
+        .pipe( istanbul( {
+            includeUntested: true
         } ) )
-        .on( 'end', cb );
-    });
+        .on( 'finish', function () {
+            gulp.src( [ 'test/**/*.js' ] )
+                .pipe( mocha() )
+                .pipe( istanbul.writeReports( {
+                    reporters: [ 'text' ]
+                } ) )
+                .on( 'end', cb );
+        });
+
 });
